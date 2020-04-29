819db1392948605cfa2031905381f9b260278bde
Chrismorales116 <50187552+Chrismorales116@users.noreply.github.com>
2020-04-29 04:57:54 -0700

    Added some more shit
    Damage knockback doesn't work the way I want yet. It's honestly pretty fuckin wonky. I want it to kind of repel you back and kill your momentum. On the plus side if you turn off the damage on the lava traps they become trampolines. WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

---
+++ Olaf the Unhuggable/Assets/Scripts/obstacleDamage.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacleDamage : MonoBehaviour
+{
+    public float damage = 1;
+    public float damageRate;
+    public float pushBackForce;
+
+    float nextDamage;
+
+    GameObject thePlayer;
+    playerHealth thePlayerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextDamage = Time.time;
+        thePlayer = GameObject.FindGameObjectWithTag("Player");
+        thePlayerHealth = thePlayer.GetComponent<playerHealth>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Attack();
+            Debug.Log("The Collision was Successful");
+        }
+    }
+
+    void Attack()
+    {
+        if(nextDamage <= Time.time)
+        {
+            thePlayerHealth.addDamage(damage);
+            nextDamage = Time.time + damageRate;
+            Debug.Log("The Attack was Successful");
+
+            pushBack(thePlayer.transform);
+        }
+    }
+
+    void pushBack(Transform pushedObject) 
+    {
+        Vector3 pushDirection = new Vector3(0, (pushedObject.position.y - transform.position.y), 0).normalized;
+        pushDirection *= pushBackForce;
+
+        Rigidbody pushedRB = pushedObject.GetComponent<Rigidbody>();
+        pushedRB.velocity = Vector3.zero;
+        pushedRB.AddForce(pushDirection, ForceMode.Impulse);
+
+        Debug.Log("The pushback was successful");
+
+    }
+}
