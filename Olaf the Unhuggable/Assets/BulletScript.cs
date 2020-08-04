bef44fa228d7cf9e9db2c558bf13d66367e869e3
Christian Morales <Chrismorales116@gmail.com>
2020-08-03 17:46:43 -0700

    Health System Works
    I feel fucking triumphant but I'm still thinkin bout that broken mess of a dash mechanic. Health system fucking works though!

---
+++ Olaf the Unhuggable/Assets/BulletScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private Rigidbody bulletRB;
+    private Vector3 bulletForce;
+    public float bulletSpeed;
+    public float damage = 1;
+    GameObject thePlayer;
+    playerHealth thePlayerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRB = GetComponent<Rigidbody>();
+        bulletForce = new Vector3(bulletSpeed, 0, 0);
+
+        thePlayer = GameObject.FindGameObjectWithTag("Player");
+        thePlayerHealth = thePlayer.GetComponent<playerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletRB.velocity = bulletForce;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            thePlayerHealth.addDamage(damage);
+            Debug.Log("The Attack was Successful");
+        }
+
+        Destroy(this.gameObject);
+    }
+}
---
+++ Olaf the Unhuggable/Assets/TurretScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform spawnLocation;
+    private Vector3 spawn;
+    private bool canSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canSpawn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canSpawn == true)
+        {
+            canSpawn = false;
+            Instantiate(bulletPrefab, spawnLocation.position, spawnLocation.rotation);
+            Debug.Log("OH FUUUUUUCK BBY I'M SPAWNING!!");
+            StartCoroutine(TurretWaiting());
+        }
+    }
+
+    IEnumerator TurretWaiting()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits however many seconds. 
+        yield return new WaitForSeconds(1);
+
+        canSpawn = true;
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+}
