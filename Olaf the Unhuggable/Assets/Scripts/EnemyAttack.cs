0b85bd80ed282d5525de7b99161ebc37e7de8d4b
Mario Victorino <mariovictorino31@gmail.com>
2021-01-06 10:40:45 -0800

    Starting Enemy Animations

---
+++ Olaf the Unhuggable/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    Animator enAnimation;
+    Rigidbody rb;
+    public GameObject Player;
+
+    bool distanceChecker = false;
+    float time = 2.0f;
+
+    void Start()
+    {
+        enAnimation = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            enAnimation.SetBool("Attack", true);
+            Debug.Log("Enter");
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            enAnimation.SetBool("Attack", false);
+            Debug.Log("Exit");
+        }
+    }
+}
